8b872dc6ba8ff91e977c7df557461e9616ae5714
uid=tongzhen,ou=people,dc=hz,dc=netease,dc=com <tongzhen@corp.netease.com>
2025-01-29 14:29:28 -0500

    change the resizing mode from nearest to bicubic and align_corners to True in torch/nn/functional.py:line 4293

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -4290,8 +4290,8 @@ def interpolate(  # noqa: F811
     input: Tensor,
     size: Optional[int] = None,
     scale_factor: Optional[List[float]] = None,
-    mode: str = "nearest",
-    align_corners: Optional[bool] = None,
+    mode: str = "bicubic", # changed from nearest to bicubic
+    align_corners: Optional[bool] = True, # changed from None to True
     recompute_scale_factor: Optional[bool] = None,
     antialias: bool = False,
 ) -> Tensor:  # noqa: B950
