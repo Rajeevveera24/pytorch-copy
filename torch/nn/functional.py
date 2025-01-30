bfe2d2fe30623fd6d2f837e0c9693dfffa6968a9
Resham Patil <reshampatil@Reshams-MacBook-Air.local>
2025-01-29 19:14:04 -0600

    Merge from merge-conflict branch after resolving merge conflict

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -4290,13 +4290,8 @@ def interpolate(  # noqa: F811
     input: Tensor,
     size: Optional[int] = None,
     scale_factor: Optional[List[float]] = None,
-<<<<<<< HEAD
-    mode: str = "bicubic",
-    align_corners: Optional[bool] = True,
-=======
     mode: str = "bilinear",
     align_corners: Optional[bool] = True,
->>>>>>> merge-conflict
     recompute_scale_factor: Optional[bool] = None,
     antialias: bool = False,
 ) -> Tensor:  # noqa: B950
