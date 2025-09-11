3650d18f01d51d56148a6659fc151965e33ed8b2
chiaszu <chiaszu.21@gmail.com>
2025-09-11 14:14:34 -0400

    change 4293 to bicubic

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -4290,7 +4290,7 @@ def interpolate(  # noqa: F811
     input: Tensor,
     size: Optional[int] = None,
     scale_factor: Optional[List[float]] = None,
-    mode: str = "bilinear",
+    mode: str = "bicubic",
     align_corners: Optional[bool] = True,
     recompute_scale_factor: Optional[bool] = None,
     antialias: bool = False,
